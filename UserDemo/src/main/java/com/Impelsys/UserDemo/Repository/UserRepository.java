f0d6d259dfb118df152bc348f96d6bdd5f266d70
Muriel Karkada <56532594+murielkarkada@users.noreply.github.com>
2021-03-22 18:19:38 +0530

    Delete UserDemo directory

--- UserDemo/src/main/java/com/Impelsys/UserDemo/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Impelsys.UserDemo.Repository;
-
-import com.Impelsys.UserDemo.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
